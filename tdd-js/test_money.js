144a8bad7d0f44d1d837b4dc28aca30a7c96e83c
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-11-29 15:11:45 +0100

    Add money

--- tdd-js/test_money.js
+++ tdd-js/test_money.js
@@ -1,5 +1,24 @@
 // IMPLEMENTATION
+class Dollar {
+  constructor(amount) {
+    this.amount = amount;
+  }
 
+  times(count) {
+    return new Dollar(this.amount * count)
+  }
+}
+
+class Money {
+  constructor (amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  times(count) {
+    return new Money(this.amount * count, this.currency);
+  }
+}
 
 
 // TEST
@@ -8,6 +27,12 @@ const assert = require('assert');
 let fiveUSD = new Dollar(5);
 let tenUSD = fiveUSD.times(2);
 assert.strictEqual(tenUSD.amount, 10);
+
+let tenEUR = new Money(10, "EUR");
+let twentyEUR = tenEUR.times(2);
+assert.strictEqual(twentyEUR.amount, 20);
+assert.strictEqual(twentyEUR.currency, "EUR");
+
 
 
 
