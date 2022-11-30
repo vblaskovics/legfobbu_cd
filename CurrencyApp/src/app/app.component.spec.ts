4f59f29dfd9453f0348f118218c05885a431a7b3
viktor.blaskovics <viktor.blaskovics@gmail.com>
2022-11-30 15:57:05 +0100

    Create unit test to new money form

--- CurrencyApp/src/app/app.component.spec.ts
+++ CurrencyApp/src/app/app.component.spec.ts
@@ -27,5 +27,41 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('CurrencyApp');
   });
 
+  describe('New money form', () => {
+    it('should be rendered', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const compiled = fixture.nativeElement as HTMLElement;
+      
+      const newMoneyForm = compiled.querySelector('[data-test="newMoneyForm"]');
+  
+      expect(newMoneyForm).toBeTruthy();
+    });
+
+    it('should add a new money to our portfolio', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const compiled = fixture.nativeElement as HTMLElement;
+
+      const amountInput = compiled.querySelector('input[name="amount"]') as HTMLInputElement;
+      expect(amountInput).withContext("Amount input field not found").toBeTruthy();
+      const currencyInput = compiled.querySelector('input[name="currency"]') as HTMLInputElement;
+      expect(currencyInput).withContext("Currency input field not found").toBeTruthy();
+
+      amountInput.value = "2";
+      currencyInput.value = "HUF";
+
+      amountInput.dispatchEvent(new Event('input'));
+      currencyInput.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      
+      const submitBtn = compiled.querySelector('[data-test="newMoneySubmit"]') as HTMLButtonElement;
+      submitBtn.click();
+      fixture.detectChanges();
+
+      const moneyItems = compiled.querySelectorAll('[data-test="portfolioMoneyItem"]');
+      expect(moneyItems.length).toBe(1);
+    })
+  
+  });
+
 
 });
